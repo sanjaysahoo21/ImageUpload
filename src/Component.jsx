0b08b8a29addd5153ab364dea2ca16a4c2555602
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2025-10-08 11:20:01 +0530

    added another image input

--- src/Component.jsx
+++ src/Component.jsx
@@ -2,24 +2,40 @@ import React, { useState } from 'react'
 
 function Component() { 
 
-    const [imagePath, SetImagePath] = useState(null);
+    const [imagePath1, SetImagePath1] = useState(null);
+    const [imagePath2, SetImagePath2] = useState(null);
 
-    const handleInput = event => {
+    const handleInput1 = event => {
         const file = event.target.files[0];
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = () => {
-          SetImagePath(reader.result);
+          SetImagePath1(reader.result);
         }
         // const path = URL.createObjectURL(file);
         // SetImagePath(path);
     }
+    const handleInput2 = event => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          SetImagePath2(reader.result);
+        }
+        // const path = URL.createObjectURL(file);
+        // SetImagePath(path);
+    }
+    
 
   return (
     <div>
-      <input type='file' onChange={(event) => handleInput(event)}/>
+      <input type='file' onChange={(event) => handleInput1(event)}/>
       <div>
-        { imagePath ? <img src={imagePath}/>:<></>}
+        { imagePath1 ? <img src={imagePath1}/>:<></>}
+      </div>
+      <input type='file' onChange={(event) => handleInput2(event)}/>
+      <div>
+        { imagePath2 ? <img src={imagePath2}/>:<></>}
       </div>
     </div>
   )
